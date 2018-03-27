4d1743937d27d4b8832e5e11c1ef522c6c37aa0b
niudazhuang <dengwm666@icloud.com>
2018-03-27 22:20:13 +0800

    skipindexjs

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -1,75 +1,94 @@
 // Karma configuration
-// Generated on Fri Mar 23 2018 02:38:50 GMT+0800 (CST)
+// Generated on Tue Mar 27 2018 13:40:46 GMT+0800 (中国标准时间)
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
 
-      // base path that will be used to resolve all patterns (eg. files, exclude)
-      basePath: '../..',
-
-      // frameworks to use
-      // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-      frameworks: ['jasmine'],
-
-      // list of files / patterns to load in the browser
-      files: [
-          'src/**/*.js',
-          'test/**/*.spec.js'
-      ],
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '../..',
 
 
-      // list of files / patterns to exclude
-      exclude: [],
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
 
-      // preprocess matching files before serving them to the browser
-      // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-      preprocessors: {
-         'src/**/*.js': ['babel'],
-         'test/**/*.spec.js': ['babel'],
-      },
+    // list of files / patterns to load in the browser
+    files: [
+      'src/praise/**/*.js',
+      'test/unit/**/*.spec.js'
+    ],
 
-      // test results reporter to use
-      // possible values: 'dots', 'progress'
-      // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-      reporters: ['progress', 'coverage'],
-      coverageReporter: {
-        dir: 'test/unit/coverage',
-        reporters: [
-          { type: 'lcov', subdir: '.' },
-          { type: 'text-summary' }
+
+    // list of files / patterns to exclude
+    exclude: [
+      'test/unit/**/*.conf.js'
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'src/praise/*.js': ['webpack', 'sourcemap', 'coverage'],
+      'test/unit/**/*.spec.js': ['webpack', 'sourcemap']
+    },
+    webpack: {
+      mode: 'development',
+      devtool: '#inline-source-map',
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            use: ['babel-loader']
+          }
         ]
-      },
+      }
+    },
+    webpackMiddleware: {
+      noInfo: true
+    },
 
 
-      // web server port
-      port: 9876,
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      dir: 'test/unit/coverage',
+      reporters: [
+        { type: 'lcov', subdir: '.' },
+        { type: 'text-summary' }
+      ]
+    },
+
+    // web server port
+    port: 9876,
 
 
-      // enable / disable colors in the output (reporters and logs)
-      colors: true,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-      // level of logging
-      // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-      logLevel: config.LOG_INFO,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-      // enable / disable watching file and executing tests whenever any file changes
-      autoWatch: false,
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
 
-      // start these browsers
-      // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-      browsers: ['PhantomJS'],
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
 
 
-      // Continuous Integration mode
-      // if true, Karma captures browsers, runs the tests and exits
-      singleRun: true,
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
 
-      // Concurrency level
-      // how many browser should be started simultaneous
-      concurrency: Infinity
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
   })
 }
